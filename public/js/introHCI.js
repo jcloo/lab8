61f4c7bfd223af76b09cbbbc2bfff9fd4874527e
Joel Loo <jcloo@ucsd.edu>
2018-03-01 10:30:21 -0800

    thirdpartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,9 +18,32 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function() {
+		$(".judge-img").bind("taphold", tapHoldHandler);
+
+		function tapHoldHandler(event) {
+			//get id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+  $('#rsvpForm').submit(function(e) {
+  	//prevents default submit + reload (we only want the submit part)
+  	e.preventDefault();
+  	console.log("submitting form...");
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	//send POST request
+  	$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+  });
+
+  function postCallback(res) {
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val(''); //clear form
+  }
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,16 @@ var data = {
 /*
  * GET tasks page.
  */
+
+ exports.addRSVP = function(req, res) {
+ 	var rsvpEmail = req.body.rsvpEmail;
+ 	//This will print in your terminal when a POST is made
+ 	console.log(rsvpEmail);
+ 	data.rsvp.push(rsvpEmail); //add to current data
+ 	//Let's send our task name back so our browser knows it worked out
+ 	res.send(rsvpEmail);
+ }
+
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
